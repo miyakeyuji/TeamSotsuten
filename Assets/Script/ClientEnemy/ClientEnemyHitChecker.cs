5ca8a1ecc3dba0f9e4b8e4388ca9338aa5bddd1d
Gai Takakura <g.takakura@outlook.com>
2015-11-25 17:20:36 +0900

    クライアント側のエネミーに必要なデータの準備

---
+++ Assets/Script/ClientEnemy/ClientEnemyHitChecker.cs
@@ -0,0 +1,20 @@
+﻿// code by Gai Takakura
+using UnityEngine;
+using System.Collections;
+
+// 衝突判定を行うクラス
+// 
+public class ClientEnemyHitChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    // 衝突判定
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
---
+++ Assets/Script/ClientEnemy/ClientEnemyOperator.cs
@@ -0,0 +1,17 @@
+﻿// code by Gai Takakura
+using UnityEngine;
+using System.Collections;
+
+// サーバーとの通信結果を受け取るクラス
+public class ClientEnemyOperator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
