c59bf7cb82ee386a256bcdcbb242e65ce047efc7
miyakeyuji <hth8.1046@gmail.com>
2015-11-25 14:41:40 +0900

    攻撃エフェクトの移動　デバイスからの情報取得を考慮したスクリプトを作成

--- Assets/Player/script/GetDeviceData.cs
+++ Assets/Player/script/GetDeviceData.cs
@@ -1,18 +1,29 @@
-﻿using UnityEngine;
+﻿///-------------------------------------------------------------------------
+///
+/// code by miyake yuji
+///
+///-------------------------------------------------------------------------
+using UnityEngine;
 using System.Collections;
 
 public class GetDeviceData : MonoBehaviour {
+    /// <summary>
+    /// デバイスの情報を保持するオブジェクトを登録する
+    /// </summary>
     [SerializeField]
     GameObject device;
+
 	// Use this for initialization
 	void Start ()
     {
+        /// デバイスの座標を取得
         gameObject.transform.position = device.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        /// デバイスの座標を取得
         gameObject.transform.position = device.transform.position;
     }
 }
