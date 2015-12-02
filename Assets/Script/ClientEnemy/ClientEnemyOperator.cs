8078799f6c84c4887d5fcbbb8272c41545b4fcb9
Gai Takakura <g.takakura@outlook.com>
2015-12-02 18:38:06 +0900

    スクリプト適用

--- Assets/Script/ClientEnemy/ClientEnemyOperator.cs
+++ Assets/Script/ClientEnemy/ClientEnemyOperator.cs
@@ -33,7 +33,6 @@ public class ClientEnemyOperator : MonoBehaviour {
     void UpdateDatas()
     {
         data = GameManager.Instance.GetEnemyData(ID);
-        //DataSet();
     }
 
     // ポジションなどの更新
