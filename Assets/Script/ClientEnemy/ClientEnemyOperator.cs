7fba1ccd78b3b5d6f3900c441180acf947812672
Gai Takakura <g.takakura@outlook.com>
2015-12-02 18:34:01 +0900

    未使用の関数をコメントアウト

--- Assets/Script/ClientEnemy/ClientEnemyOperator.cs
+++ Assets/Script/ClientEnemy/ClientEnemyOperator.cs
@@ -24,14 +24,10 @@ public class ClientEnemyOperator : MonoBehaviour {
     EnemyMasterData data = null;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	//void Start () {}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	//void Update () {}
 
     // ポジションなどの更新
     void UpdateDatas()
