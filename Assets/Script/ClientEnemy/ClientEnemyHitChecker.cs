3fd2f1067849ef233c137802ff8e7391598e974c
Gai Takakura <g.takakura@outlook.com>
2015-12-02 16:49:05 +0900

    update

--- Assets/Script/ClientEnemy/ClientEnemyHitChecker.cs
+++ Assets/Script/ClientEnemy/ClientEnemyHitChecker.cs
@@ -1,9 +1,20 @@
-﻿// code by Gai Takakura
+﻿//-------------------------------------------------------------
+// 衝突のチェックを行うクラス
+//
+// code by Gai Takakura
+//-------------------------------------------------------------
 using UnityEngine;
 using System.Collections;
 
 // 衝突判定を行うクラス
 public class ClientEnemyHitChecker : MonoBehaviour {
+    ClientEnemyOperator　enemyOperator = null;
+
+    void Start()
+    {
+        enemyOperator = this.gameObject.GetComponent<ClientEnemyOperator>();
+    }
+
     // 衝突判定
     void OnTriggerEnter(Collider other)
     {
