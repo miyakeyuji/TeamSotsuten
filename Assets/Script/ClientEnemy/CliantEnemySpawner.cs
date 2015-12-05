51300622dda53cabe515e387b18c9b3a8ae54579
Gai Takakura <g.takakura@outlook.com>
2015-12-05 18:01:00 +0900

    生成処理を追加

--- Assets/Script/ClientEnemy/CliantEnemySpawner.cs
+++ Assets/Script/ClientEnemy/CliantEnemySpawner.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿//-------------------------------------------------------------
+// サーバーから送られたデータを処理するクラス
+//
+// code by Gai Takakura
+//-------------------------------------------------------------
+using UnityEngine;
 using System.Collections;
 
 public class CliantEnemySpawner : MonoBehaviour {
@@ -6,12 +11,26 @@ public class CliantEnemySpawner : MonoBehaviour {
     [SerializeField]
     GameObject parentObject = null;
 
-	// Update is called once per frame
-	void Update ()
+    [SerializeField]
+    GameObject enemy = null;
+
+    // Update is called once per frame
+    void Update ()
     {
 	    if(Input.GetMouseButtonDown(0))
         {
             
         }
 	}
+
+    /// <summary>
+    /// エネミーを生成する
+    /// </summary>
+    /// <param name="_id">設定するID(配列のindex)</param>
+    void SpawnEnemy(int _id)
+    {
+        var data = GameManager.Instance.GetEnemyData(_id);
+        var createEnemy = (GameObject)Instantiate(enemy, data.Position, Quaternion.Euler(data.Rotation));
+        if(parentObject != null) createEnemy.transform.parent = parentObject.transform;   
+    }
 }
