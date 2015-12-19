99714a5ee7118316dfea336f98189cfb69a02abb
syougun360 <syougun360@infoseek.jp>
2015-12-19 21:35:38 +0900

    モーション種類関係のエラー回避、シーン各コンポーネントのオブジェクトのアタッチ

--- Assets/Script/Player/EffectCreate.cs
+++ Assets/Script/Player/EffectCreate.cs
@@ -15,18 +15,26 @@ public class EffectCreate : MonoBehaviour
         GameObject checkObject = null;
         switch (skillType)
         {
-            case MotionManager.MotionSkillType.HORIZONTAL_LEFT_RIGHT:
-                checkObject = transform.FindChild("LeftRight").gameObject;
+            case MotionManager.MotionSkillType.STRENGTH:
+
                 break;
-            case MotionManager.MotionSkillType.HORIZONTAL_RIGHT_LEFT:
-                checkObject = transform.FindChild("RightLeft").gameObject;
+
+            case MotionManager.MotionSkillType.WEAK:
+
                 break;
-            case MotionManager.MotionSkillType.VERTICAL_DOWN_UP:
-                checkObject = transform.FindChild("DownUp").gameObject;
-                break;
-            case MotionManager.MotionSkillType.VERTICAL_UP_DOWN:
-                checkObject = transform.FindChild("UpDown").gameObject;
-                break;
+
+            //case MotionManager.MotionSkillType.HORIZONTAL_LEFT_RIGHT:
+            //    checkObject = transform.FindChild("LeftRight").gameObject;
+            //    break;
+            //case MotionManager.MotionSkillType.HORIZONTAL_RIGHT_LEFT:
+            //    checkObject = transform.FindChild("RightLeft").gameObject;
+            //    break;
+            //case MotionManager.MotionSkillType.VERTICAL_DOWN_UP:
+            //    checkObject = transform.FindChild("DownUp").gameObject;
+            //    break;
+            //case MotionManager.MotionSkillType.VERTICAL_UP_DOWN:
+            //    checkObject = transform.FindChild("UpDown").gameObject;
+            //    break;
             default:
                 break;
         }
